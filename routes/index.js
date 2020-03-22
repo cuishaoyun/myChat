2a6efb87772a4234663ada1db777348325a62fea
cuishaoyun <1571609683@@qq.com>
2020-03-22 16:49:28 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const filePath = 'C:/Users/zack/Desktop/myChat/views/';
+const filePath = 'views/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
